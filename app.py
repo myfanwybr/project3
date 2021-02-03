0a6b6c6902969ae8ef0df8986d5721145e7db11c
myfanwybr <m.brownrobinson@hotmail.com>
2021-02-02 20:16:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+#import dependencies
 from flask import Flask, jsonify, render_template, redirect
 from flask_pymongo import PyMongo
+
+##demo dictionary
+
+my_dict={
+    1: "catherine", 
+    2: "myfanwy", 
+    3: "rubal", 
+    4: "manisha", 
+    5: "sharanvika"
+
+}
 
 app=Flask(__name__)
 
@@ -9,6 +21,11 @@ def home():
     return render_template("index.html")
 
 ##service routes
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route("/api/prices")
+def api_prices():
+    return jsonify(my_dict)
+
+#run app
+#if __name__=="__main__":
+app.run(debug=True)
 
