066b9ba0aee12e334b64acdfc42b4bb8568bb8de
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-02-06 10:09:23 -0700

    Update weather.js

---
+++ bikeshare/static/js/weather.js
@@ -0,0 +1,16 @@
+function getWeather(city){
+	if(city){
+	var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){
+		
+	}
+	}
+}
+
+function formatWeather(data){
+	return
+}
+
+function getForecast(city,days){
+	
+}
