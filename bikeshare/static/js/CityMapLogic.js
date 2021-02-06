3b6d67e9a56119ade80ae01ae4bb658a9da1d21c
manishalal145 <56851902+manishalal145@users.noreply.github.com>
2021-02-06 11:05:21 -0500

    coding

--- bikeshare/static/js/CityMapLogic.js
+++ bikeshare/static/js/CityMapLogic.js
@@ -24,4 +24,39 @@ function createMap(bikeStations) {
       zoom: 12,
       layers: [lightmap, bikeStations]
     });
+
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
+
+function createMarkers(response) {
+
+    // Pull the "stations" property off of response.data
+    var stations = response.data.stations;
   
+    // Initialize an array to hold bike markers
+    var bikeMarkers = [];
+  
+    // Loop through the stations array
+    for (var index = 0; index < stations.length; index++) {
+      var station = stations[index];
+  
+      // For each station, create a marker and bind a popup with the station's name
+      var bikeMarker = L.marker([station.lat, station.lon])
+        .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+  
+      // Add the marker to the bikeMarkers array
+      bikeMarkers.push(bikeMarker);
+    }
+  
+    // Create a layer group made from the bike markers array, pass it into the createMap function
+    createMap(L.layerGroup(bikeMarkers));
+  }
+  
+  
+  // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
+  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", createMarkers);
+  
+
