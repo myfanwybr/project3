a4c2bf2d502d0521db357948a7a09b7054bc932e
myfanwybr <m.brownrobinson@hotmail.com>
2021-02-06 14:04:47 -0500

    app.js push

--- bikeshare/static/js/app.js
+++ bikeshare/static/js/app.js
@@ -1,7 +1,21 @@
 
-app.use("/static", express.static('./static/'));
+//app.use("/static", express.static('./static/'));
 
 
 d3.json("/api/prices").then(function(data) {
+    
+    var data=data
     console.log(data)
+    data.forEach(function(aliens){
+        var row=tbody.append("tr")
+    
+        //select objects using . Entries
+        Object.entries(aliens).forEach(function([key, value]){
+            var cell=row.append("td")
+            cell.text(value)
+        })
+    })
 })
+
+var tbody= d3.select("tbody")
+
