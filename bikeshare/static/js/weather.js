89f76efd4be4967f8e72176fe8d28219d9359c0a
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2021-02-06 10:14:39 -0700

    Update weather.js

--- bikeshare/static/js/weather.js
+++ bikeshare/static/js/weather.js
@@ -2,9 +2,15 @@ function getWeather(city){
 	if(city){
 	var xhr=new XMLHttpRequest();
 	xhr.onreadystatechange=function(){
-		
-	}
-	}
+		if(this.station==200 && this.readyState==4){
+			var formattedData=formatWeather(JSON.parse(xhr.responseText));
+			document.getElementById("weather-data").innerHTML=formattedData;
+			document.getElementById('cityname').value="";
+
+		}
+	};
+	xhr.open("GET","http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=d610395e85b50074b834a0234b0776db");
+	xhr.send();
 }
 
 function formatWeather(data){
@@ -12,5 +18,5 @@ function formatWeather(data){
 }
 
 function getForecast(city,days){
-	
+
 }
