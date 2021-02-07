95982b0618554a733d71ad8c57c09b7655bdb6c3
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-02-06 23:57:53 -0500

    Update plots.js

--- bikeshare/static/js/plots.js
+++ bikeshare/static/js/plots.js
@@ -1,3 +1,38 @@
-d3.json("/api/visualize").then((date) => {
-    console.log(data);
-})
+d3.json("/api/visualize").then(function(data) {
+    
+    var data = data;
+    start_hour = [];
+    count_hour = [];
+
+    Object.entries(data).forEach(([key, value]) => {
+        console.log(key, value);
+        count_hour.push(value.hourly_trip_count);
+        start_hour.push(value.start_hour);
+    });
+
+    console.log(start_hour);
+    console.log(count_hour);
+
+    var dataX = [{
+        x: start_hour,
+        y: count_hour,
+        text: start_hour,
+        mode: "markers",
+        marker: {
+            color: count_hour,
+            size: count_hour,
+            colorscale: "Jet"
+        }
+    }];
+
+    var layout1 = {
+        xaxis: {title: "Time of Day"},
+        hovermode: "closest",
+        showlegend: false
+    };
+
+    Plotly.newPlot("bubble-time", dataX, layout1);
+
+});
+
+
