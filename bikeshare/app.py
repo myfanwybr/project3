2386d5fc34c18e5a6f5523fcea5625533cd93640
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-02-13 12:02:40 -0500

    creds

--- bikeshare/app.py
+++ bikeshare/app.py
@@ -14,7 +14,17 @@ bigquery_uri = f'bigquery://{gcp_project}/{bigquery_dataset}'
 
 app=Flask(__name__)
 
-credentials = service_account.Credentials.from_service_account_file('bikeshare-303620-09b8e6c22e54.json')
+# the json credentials stored as env variable
+json_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+
+# generate json - if there are errors here remove newlines in .env
+json_data = json.loads(json_str)
+# the private_key needs to replace \n parsed as string literal with escaped newlines
+json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
+
+# use service_account to generate credentials object
+credentials = service_account.Credentials.from_service_account_info(json_data)
+#credentials = service_account.Credentials.from_service_account_file('bikeshare-303620-09b8e6c22e54.json')
 
 ##front end routes
 @app.route("/")
