430a245a2228163f6ee33b497cf093e3817a58a4
myfanwybr <m.brownrobinson@hotmail.com>
2021-02-15 21:43:26 -0500

    Update app.py

--- bikeshare/app.py
+++ bikeshare/app.py
@@ -39,8 +39,8 @@ json_data = {
 
 
 # use service_account to generate credentials object
-credentials = service_account.Credentials.from_service_account_info(json_data)
-# credentials = service_account.Credentials.from_service_account_file('bikeshare.json')
+credentials = service_account.Credentials.from_service_account_file(json_data)
+#credentials = service_account.Credentials.from_service_account_file('bikeshare.json')
 
 ##front end routes
 @app.route("/")
