ce81f8cae910572adef26249aa8b073bc5bb7122
manishalal145 <56851902+manishalal145@users.noreply.github.com>
2021-02-17 13:29:18 -0500

    changes

--- bikeshare/static/js/CityMapLogic.js
+++ bikeshare/static/js/CityMapLogic.js
@@ -78,14 +78,14 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
-  accessToken: API_KEY
+  accessToken: "pk.eyJ1IjoibWFuaXNoYWxhbCIsImEiOiJja2tlc3Y5eGcwMzV0MnZxZThvb3g3d3MwIn0.DPmfdzNu4SykIHWqzFoYjA"
 });
 
 var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "dark-v10",
-  accessToken: API_KEY
+  accessToken: "pk.eyJ1IjoibWFuaXNoYWxhbCIsImEiOiJja2tlc3Y5eGcwMzV0MnZxZThvb3g3d3MwIn0.DPmfdzNu4SykIHWqzFoYjA"
 });
 
 // Define a baseMaps object to hold our base layers
